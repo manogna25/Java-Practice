9102eb6c1413d518f9b18a9b94b8eca015248d8a
manogna25 <132990279+manogna25@users.noreply.github.com>
2025-03-11 17:58:02 +0530

    Eg.java

--- 53e/Eg.java
+++ 53e/Eg.java
@@ -5,10 +5,21 @@ public class Eg {
         Scanner sc=new Scanner(System.in);
         char ch=sc.next().charAt(0);
         int n=ch;
-        if(n>=((int)'a') && n<=((int)'z')){
+        /*if(n>=((int)'a') && n<=((int)'z')){
             System.out.println(0);
         }
         else if(n>=((int)'A') && n<=((int)'Z')){
+            System.out.println(1);
+        }
+        else{
+            System.out.println(-1);
+        }
+        sc.close();
+        */
+        if(Character.isLowerCase(ch)){
+            System.out.println(0);
+        }
+        else if(Character.isUpperCase(ch)){
             System.out.println(1);
         }
         else{
