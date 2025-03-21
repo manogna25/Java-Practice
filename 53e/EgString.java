197e5e8e0785aa70b70cf822c9e3a4458f4c72b5
manogna25 <132990279+manogna25@users.noreply.github.com>
2025-03-21 11:40:45 +0530

    EgString.java

--- 53e/EgString.java
+++ 53e/EgString.java
@@ -49,5 +49,143 @@ public class EgString {
         d=d.concat(c);
         d=d.concat(b);
         System.out.println(d);
+        Scanner sc=new Scanner(System.in);
+        //Replace character
+        /*String str=sc.next();
+        char c1=sc.next().charAt(0);
+        char c2=sc.next().charAt(0);
+        String str1="";
+        //String str2=str.replace(c1,c2);
+        //System.out.println(str2);
+        
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==c1){
+                str1=str1+c2;
+            }
+            else{
+                str1=str1+str.charAt(i);
+            }
+        }
+        System.out.println(str1);*/
+        //remove character
+        /*String str=sc.next();
+        String s="";
+        char ch=sc.next().charAt(0);
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==ch){
+                continue;
+            }
+            else{
+                s=s+str.charAt(i);
+            }
+        }
+        System.out.println(s);*/
+        //remove spaces in between 
+        /*String str=sc.nextLine();
+        String s="";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==' '){
+                continue;
+            }
+            else{
+                s=s+str.charAt(i);
+            }
+        }
+        System.out.println(s);*/
+        //string sentence reverse
+        /*String str=sc.nextLine();
+        String s[]=str.split(" ");
+        String s1="";
+        for(int i=s.length-1;i>=0;i--){
+            s1=s1+s[i]+" ";
+        }
+        System.out.println(s1.trim());*/
+        //i am good ==> I Am Good
+        /*String str=sc.nextLine();
+        String s[]=str.split(" ");
+        String s1="";
+        for(int i=0;i<s.length;i++){
+            //s[i]=s[i].substring(0,1).toUpperCase()+s[i].substring(1).toLowerCase();
+            s[i]=Character.toUpperCase(s[i].charAt(0))+s[i].substring(1).toLowerCase();
+        }
+        for(int i=0;i<s.length;i++){
+            s1=s1+s[i]+" ";
+        }
+        System.out.println(s1.trim());
+        */
+        //palindrome
+        /*String str=sc.next();
+        String s="";
+        for(int i=str.length()-1;i>=0;i--){
+            s=s+str.charAt(i);
+        }
+        if(s.equals(str)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }*/
+        /*String str=sc.next();
+        boolean b=true;
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                b=false;
+                break;
+            }
+        }
+        if(b){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("not a palindrome");
+        }*/
+        //score of a string
+        /*String str=sc.next();
+        int score=0;
+        for(int i=0;i<str.length()-1;i++){
+            score+=Math.abs((int)(str.charAt(i))-(int)(str.charAt(i+1)));
+        }
+        System.out.println("score of string \""+str+"\" is "+score);
+        */
+        /*String str=sc.next();
+        char c1='.';
+        String c2="[.]";
+        String s="";
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)==c1){
+                s+=c2;
+            }
+            else{
+                s+=str.charAt(i);
+            }
+        }
+        System.out.println(s);*/
+        /*int n=sc.nextInt();
+        int x=0;
+        String[] arr=new String[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.next();
+        }
+        for(String a:arr){
+            if(a.equals("X--")||a.equals("--X")){
+                x=x-1;
+            }
+            if(a.equals("X++")||a.equals("++X")){
+                x=x+1;
+            }
+        }
+        System.out.println(x);*/
+        /*int n=sc.nextInt();
+        int count=0;
+        while(n!=0){
+            count++;
+            if(n%2==0){
+                n/=2;
+            }
+            else{
+                n-=1;
+            }
+        }
+        System.out.println("No of steps: "+count);*/
     }
 }
